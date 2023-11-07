1ed34a0e12b69049a6db2b8bc70b3bf23a198289
Jerome Haltom <jhaltom@alethic.solutions>
2023-11-07 15:48:43 -0600

    Minor.

--- Cogito.Extensions.Configuration.Autofac.Sample1/Startup.cs
+++ Cogito.Extensions.Configuration.Autofac.Sample1/Startup.cs
@@ -1,10 +1,6 @@
-﻿using Cogito.Autofac;
-
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-using Serilog;
 
 namespace Cogito.Extensions.Configuration.Autofac.Sample1
 {
